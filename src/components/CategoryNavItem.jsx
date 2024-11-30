dc2f0a62ef159883b085b1c001d4e9efa2f72e4e
Steve Paul <steveebenezer@protonmail.com>
2024-11-30 16:37:24 +0530

    Fix formatting

--- src/components/CategoryNavItem.jsx
+++ src/components/CategoryNavItem.jsx
@@ -4,64 +4,64 @@ import data from "../data/tools.json";
 import "./CategoryNavItem.css";
 
 export default function CategoryNavItem(props) {
-	const buttonRef = useRef(null);
-	const { title, category, filter } = props;
-	const [isActive, setIsActive] = useState(false);
+  const buttonRef = useRef(null);
+  const { title, category, filter } = props;
+  const [isActive, setIsActive] = useState(false);
 
-	const handleNavigation = (e) => {
-		e.preventDefault();
-		navigate(`/categories/${category}`, {
-			history: "push",
-			state: { category },
-		});
-	};
+  const handleNavigation = (e) => {
+    e.preventDefault();
+    navigate(`/categories/${category}`, {
+      history: "push",
+      state: { category },
+    });
+  };
 
-	const getCategoryCount = () => {
-		if (category === "all") {
-			return data.tools.reduce((acc, item) => acc + item.content.length, 0);
-		}
+  const getCategoryCount = () => {
+    if (category === "all") {
+      return data.tools.reduce((acc, item) => acc + item.content.length, 0);
+    }
 
-		const navItemData = data.tools.filter((item) => item.category === category);
-		return navItemData[0]?.content.length;
-	};
+    const navItemData = data.tools.filter((item) => item.category === category);
+    return navItemData[0]?.content.length;
+  };
 
-	useEffect(() => {
-		let subscription = true;
+  useEffect(() => {
+    let subscription = true;
 
-		if (filter === category) {
-			setIsActive(true);
-		} else {
-			setIsActive(false);
-		}
+    if (filter === category) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
 
-		return () => (subscription = !subscription);
-	}, [filter]);
+    return () => (subscription = !subscription);
+  }, [filter]);
 
-	useEffect(() => {
-		let subscription = true;
-		const activeButton = buttonRef.current.classList.contains("is-active");
+  useEffect(() => {
+    let subscription = true;
+    const activeButton = buttonRef.current.classList.contains("is-active");
 
-		if (activeButton) {
-			buttonRef.current.scrollIntoView({
-				behavior: "instant",
-				block: "nearest",
-				inline: "center",
-			});
-		}
+    if (activeButton) {
+      buttonRef.current.scrollIntoView({
+        behavior: "instant",
+        block: "nearest",
+        inline: "center",
+      });
+    }
 
-		return () => (subscription = !subscription);
-	}, [isActive]);
+    return () => (subscription = !subscription);
+  }, [isActive]);
 
-	return (
-		<button
-			ref={buttonRef}
-			onClick={handleNavigation}
-			className={`nav__item nu-u-text--secondary-alt nu-c-fs-normal nu-u-py-5 nu-u-px-0 nu-u-me-8 nav__item--filter ${
-				isActive ? "is-active" : ""
-			}`}
-			dangerouslySetInnerHTML={{
-				__html: `${title} (${getCategoryCount()})`,
-			}}
-		></button>
-	);
+  return (
+    <button
+      ref={buttonRef}
+      onClick={handleNavigation}
+      className={`nav__item nu-u-text--secondary-alt nu-c-fs-normal nu-u-py-5 nu-u-px-0 nu-u-me-8 nav__item--filter ${
+        isActive ? "is-active" : ""
+      }`}
+      dangerouslySetInnerHTML={{
+        __html: `${title} (${getCategoryCount()})`,
+      }}
+    ></button>
+  );
 }
